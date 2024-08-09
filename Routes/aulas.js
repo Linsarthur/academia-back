92b0b032a4a58f40f8bc750fd49b7a95fa3ac152
rayssasantana <rayssasantana.br@gmail.com>
2024-08-09 12:20:40 -0300

    Alterações finais

--- Routes/aulas.js
+++ Routes/aulas.js
@@ -7,71 +7,79 @@ export const aulasRouter = Router();
 
 
 aulasRouter.get("/aulas", async (req, res) => {
-    const listaAulas = await Aula.findAll();
-    res.json(listaAulas);
+  const listaAulas = await Aula.findAll();
+  res.json(listaAulas);
+});
+
+aulasRouter.get("/aulas/:id", async (req, res) => {
+  const aula = await Aula.findOne({
+    where: { id: req.params.id },
+    include: [{ model: Instrutor, attributes: ["id", ["nome", "nomeInstrutor"]] },
+    { model: Aluno, attributes: ["id", ["nome", "nomeAluno"]] }]
   });
-  
-  aulasRouter.get("/aulas/:id", async (req, res) => {
-    const aula = await Aula.findOne({
-      where: { id: req.params.id },
-      include: [{ model: Instrutor, attributes: ["id", ["nome", "nomeInstrutor"]] },
-      { model: Aluno, attributes: ["id", ["nome", "nomeAluno"]] }]
-    });
-  
+
+  if (aula) {
+    res.json(aula);
+  } else {
+    res.status(404).json({ message: "Aula não encontrada." });
+  }
+});
+
+aulasRouter.delete("/aulas/:id", async (req, res) => {
+  try {
+    const aula = await Aula.findByPk(req.params.id);
     if (aula) {
-      res.json(aula);
+      await aula.destroy();
+      res.json({ message: "Aula removida com sucesso" });
     } else {
       res.status(404).json({ message: "Aula não encontrada." });
     }
-  });
-  
-  aulasRouter.delete("/aulas/:id", async (req, res) => {
-    try {
-      const aula = await Aula.findByPk(req.params.id);
+  } catch (err) {
+    res.status(500).json({ message: "Ocorreu um erro ao remover a aula" });
+  }
+});
+
+aulasRouter.post("/aulas", async (req, res) => {
+  const { nomeAula, data, horario, nivel, alunoId, instrutorId } = req.body;
+
+  try {
+    const aluno = await Aluno.findByPk(alunoId);
+    const instrutor = await Instrutor.findByPk(instrutorId);
+
+    if (aluno && instrutor) {
+      await Aula.create({ nomeAula, data, horario, nivel, alunoId, instrutorId });
+      res.json({ message: "Aula marcada com sucesso." });
+    } else {
+      res
+        .status(404)
+        .json({ message: "Falha ao marcar aula." });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Ocorreu um erro ao marcar aula." });
+  }
+});
+
+aulasRouter.put("/aulas/:id", async (req, res) => {
+  const { nomeAula, data, horario, nivel, alunoId, instrutorId } = req.body;
+
+  try {
+    const aula = await Aula.findByPk(req.params.id);
+    const aluno = await Aluno.findByPk(alunoId);
+    const instrutor = await Instrutor.findByPk(instrutorId);
+
+    if (aluno && instrutor) {
       if (aula) {
-        await aula.destroy();
-        res.json({ message: "Aula removida com sucesso" });
+        await aula.update({ nomeAula, data, horario, nivel, alunoId, instrutorId });
+        res.json({ message: "Aula atualizada com sucesso." });
       } else {
         res.status(404).json({ message: "Aula não encontrada." });
       }
-    } catch (err) {
-      res.status(500).json({ message: "Ocorreu um erro ao remover a aula" });
+    } else {
+      res
+        .status(404)
+        .json({ message: "Falha ao marcar aula." });
     }
-  });
-  
-  aulasRouter.post("/aulas", async (req, res) => {
-    const { nomeAula, data, horario, nivel, alunoId, instrutorId } = req.body;
-  
-    try {
-      const aluno = await Aluno.findByPk(alunoId);
-      const instrutor = await Instrutor.findByPk(instrutorId);
-  
-      if (aluno, instrutor) {
-        await Aula.create({ nomeAula, data, horario, nivel, alunoId, instrutorId });
-        res.json({ message: "Aula marcada com sucesso." });
-      } else {
-        res
-          .status(404)
-          .json({ message: "Falha ao marcar aula." });
-      }
-    } catch (err) {
-      res.status(500).json({ message: "Ocorreu um erro ao marcar aula." });
-    }
-  });
-  
-   aulasRouter.put("/aulas/:id", async (req, res) => {
-     const { nomeAula, data, horario, nivel } = req.body;
-  
-     try {
-       const aula = await Aula.findByPk(req.params.id);
-       if (aula) {
-         await aula.update({ nomeAula, data, horario, nivel });
-         res.json({message: "Aula atualizada com sucesso."});
-       } else {
-         res.status(404).json({ message: "Aula não encontrada." });
-       }
-    } catch (err) {
-      res.status(500).json({message: "Um erro ocorreu ao atualizar a aula."});
-    }
-   });
-  
+  } catch (err) {
+    res.status(500).json({ message: "Um erro ocorreu ao atualizar a aula." });
+  }
+});
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { alunosRouter } from "./Routes/alunos.js"
 import { instrutoresRouter } from "./Routes/instrutores.js"
 import { aulasRouter } from "./Routes/aulas.js";
+import cors from "cors";
 
 authenticate(connection).then(() => {
   connection.sync();
@@ -11,6 +12,8 @@ authenticate(connection).then(() => {
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({origin: "http://localhost:5173"}));
 
 app.use(alunosRouter);
 app.use(instrutoresRouter);
